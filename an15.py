c852a4f8c1cf47bc570d244b3b3f7d49b44d004d
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:56:47 +0530

    to unpack a tuple into variables

---
+++ an15.py
@@ -0,0 +1,9 @@
+# Python program to unpack a tuple into variables
+tuple = (10, 20, 30)
+
+green, red, blue = tuple
+
+print("Tuple:", tuple)
+print("green =", green)
+print("red =", red)
+print("blue=", blue)
