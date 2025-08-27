8127f542599b1291f5a09a8ccf675296f7a2e8ec
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:46:03 +0530

    to find maximum and minimum value in a set

---
+++ an6.py
@@ -0,0 +1,11 @@
+# Python program to find maximum and minimum value in a set
+
+n = {5, 12, 3, 18, 7, 25, 1}
+
+print("The set is:",n)
+
+max_value = max(n)
+min_value = min(n)
+
+print("Maximum value in the set:", max_value)
+print("Minimum value in the set:", min_value)
