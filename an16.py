cec629b9da12906ba8556eb295d8dad8b26104e5
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:58:11 +0530

    to get the 4th element from front & last 4th of tuple

---
+++ an16.py
@@ -0,0 +1,12 @@
+# Python program to get the 4th element from front and 4th element from last of a tuple
+
+tuple = (10, 20, 30, 40, 50, 60, 70, 80)
+
+print("Tuple:", tuple)
+
+front_4th = tuple[3]
+
+last_4th = tuple[-4]
+
+print("4th element from the front:", front_4th)
+print("4th element from the last:", last_4th)
