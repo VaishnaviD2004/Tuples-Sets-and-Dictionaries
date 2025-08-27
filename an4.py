abc03517214cdba7ddce08865ea38f864878425f
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:44:02 +0530

    create a tuple with numbers and print one item

---
+++ an4.py
@@ -0,0 +1,5 @@
+# Python program to create a tuple with numbers and print one item
+numbers = (10, 20, 30, 40, 50)#tuple with numbers
+
+print("The tuple is:", numbers)
+print("One item from the tuple:", numbers[3])# Print one item
