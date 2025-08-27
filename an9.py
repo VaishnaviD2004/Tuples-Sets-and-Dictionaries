07ee608dedbf7750909d7814494f6049ef792dc7
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:48:12 +0530

    To create an intersection of sets

---
+++ an9.py
@@ -0,0 +1,9 @@
+# Python program to create an intersection of sets
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
+
+print("Set 1:", s1)
+print("Set 2:", s2)
+intersection_set = s1.intersection(s2) 
+
+print("Intersection of S1 and S2:", intersection_set)
