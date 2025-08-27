95ba2db7ffea5a7497714630e006f0a699fa202d
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:43:06 +0530

    to find the length of a set

---
+++ an3.py
@@ -0,0 +1,8 @@
+# Python program to find the length of a set
+
+myset = {"atharv", "baban", "jerry", "maya"} # Create a set
+
+length = len(myset) # Find length using len()
+
+print("The set is:", myset)
+print("Length of the set:", length)
