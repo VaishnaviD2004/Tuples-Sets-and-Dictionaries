a4335c1c513c8fc983ff7344373cf562652c36ee
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:51:36 +0530

    to create a union of sets

---
+++ an10.py
@@ -0,0 +1,9 @@
+# Python program to create a union of sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+union_set = set1.union(set2)
+
+print("Union of Set1 and Set2:", union_set)
