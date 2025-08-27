64d2ecdb6d3da6a32a1f3aa48116d1277a6fd2f3
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:47:33 +0530

    to convert a tuple to a string

---
+++ an8.py
@@ -0,0 +1,6 @@
+# Python program to convert a tuple to a string
+my_tuple = ('o', 't', 'j', 'g', 'e', 'n')
+print("Original Tuple:", my_tuple)
+result = ''.join(my_tuple)# Convert tuple to string using join()
+
+print("Tuple converted to string:", result)
