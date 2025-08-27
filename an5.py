419799bfd9dbddc865d7979fa08ce65f1d769add
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-27 21:45:20 +0530

    to add a key to a dictionary

---
+++ an5.py
@@ -0,0 +1,7 @@
+# Python script to add a key to a dictionary
+y_dict = {0: 10, 1: 20}
+print("Original Dictionary:", y_dict)
+
+y_dict[2] = 30 # Adding a new key-value pair
+
+print("Updated Dictionary:", y_dict)
